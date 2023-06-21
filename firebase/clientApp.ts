7277e2175d77dfe9cf5ecb247564783ac82ab3bd
Luis Guilherme Pelin Martins <lgpelin92@gmail.com>
2023-06-21 14:39:24 -0300

    FEA : Add Firebase client and LLLG API utilities
    - Add firebase initialization with configuration in firebase/clientApp.ts
    - Implement LLLG API utility functions such as:
      - uploadSource
      - listSources
      - deleteSource
      - createContext
      - listContexts
      - deleteContext
      - createVectorStore
      - listVectorStores
      - deleteVectorStore
      - vectorStoreQuery
    
    this commit was generated by lguibr/comai

---
+++ firebase/clientApp.ts
@@ -0,0 +1,17 @@
+import { getApps, initializeApp } from "firebase/app";
+
+import "firebase/auth";
+import "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
+
+// Initialize Firebase
+export default app;
---
+++ lib/lllg.ts
@@ -0,0 +1,260 @@
+// utils/api.ts
+
+interface UploadSourcePayload {
+  source: File; // or whatever the type of your source is
+}
+
+export async function uploadSource(
+  payload: UploadSourcePayload,
+  token: string
+): Promise<Response | Error> {
+  try {
+    const formData = new FormData();
+    formData.append("file", payload.source);
+
+    const response = await fetch("/api/sources", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      body: formData,
+    });
+
+    return response;
+  } catch (e) {
+    console.log(e);
+    return e as Error;
+  }
+}
+
+export async function listSources(token: string): Promise<string[] | Error> {
+  try {
+    const response = await fetch("/api/sources", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Error while fetching sources");
+    }
+
+    const data = await response.json();
+
+    return data.files;
+  } catch (e) {
+    console.log(e);
+    return e as Error;
+  }
+}
+
+export async function deleteSource(
+  fileName: string,
+  token: string
+): Promise<Response | Error> {
+  try {
+    const response = await fetch(`/api/sources/${fileName}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Error while deleting source");
+    }
+
+    return response;
+  } catch (e) {
+    console.log(e);
+    return e as Error;
+  }
+}
+
+export interface CreateContextPayload {
+  name: string;
+  description: string;
+  files: string[];
+}
+
+export async function createContext(
+  payload: CreateContextPayload,
+  token: string
+): Promise<Response | Error> {
+  try {
+    const response = await fetch("/api/contexts", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    });
+
+    if (!response.ok) {
+      throw new Error("Error while creating context");
+    }
+
+    return response;
+  } catch (e) {
+    console.log(e);
+    return e as Error;
+  }
+}
+
+export async function listContexts(token: string): Promise<any[] | Error> {
+  try {
+    const response = await fetch("/api/contexts", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Error while fetching contexts");
+    }
+
+    const data = await response.json();
+
+    return data.contexts;
+  } catch (e) {
+    console.log(e);
+    return e as Error;
+  }
+}
+
+export async function deleteContext(
+  contextName: string,
+  token: string
+): Promise<Response | Error> {
+  try {
+    const response = await fetch(`/api/contexts/${contextName}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Error while deleting context");
+    }
+
+    return response;
+  } catch (e) {
+    console.log(e);
+    return e as Error;
+  }
+}
+
+export interface CreateVectorStorePayload {
+  name: string;
+  description: string;
+  contexts: string[];
+}
+
+export async function createVectorStore(
+  payload: CreateVectorStorePayload,
+  token: string
+): Promise<Response | Error> {
+  try {
+    const response = await fetch("/api/vectorstore", {
+      // update the path as per your server endpoint
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    });
+
+    if (!response.ok) {
+      throw new Error("Error while creating vector store");
+    }
+
+    return response;
+  } catch (e) {
+    console.log(e);
+    return e as Error;
+  }
+}
+
+export async function listVectorStores(token: string): Promise<any[] | Error> {
+  try {
+    const response = await fetch("/api/vectorstore", {
+      // update the path as per your server endpoint
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Error while fetching vector stores");
+    }
+
+    const data = await response.json();
+
+    return data.vectorstores; // make sure the key matches what your server returns
+  } catch (e) {
+    console.log(e);
+    return e as Error;
+  }
+}
+
+export async function deleteVectorStore(
+  vectorstoreName: string,
+  token: string
+): Promise<Response | Error> {
+  try {
+    const response = await fetch(`/api/vectorstore/${vectorstoreName}`, {
+      // update the path as per your server endpoint
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Error while deleting vector store");
+    }
+
+    return response;
+  } catch (e) {
+    console.log(e);
+    return e as Error;
+  }
+}
+
+export interface VectorStoreQueryPayload {
+  query: string;
+}
+
+export async function vectorStoreQuery(
+  vectorStoreName: string,
+  payload: VectorStoreQueryPayload,
+  token: string
+): Promise<any | Error> {
+  try {
+    const response = await fetch(`/api/vectorstore/${vectorStoreName}/query`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    });
+
+    if (!response.ok) {
+      throw new Error("Error while querying vector store");
+    }
+
+    const data = await response.json();
+
+    return data;
+  } catch (e) {
+    console.log(e);
+    return e as Error;
+  }
+}
