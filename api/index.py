ad9074a6ce73d70f7338db43135e46219187fc95
Luis Guilherme Pelin Martins <lgpelin92@gmail.com>
2023-06-21 14:37:59 -0300

    ENH: Refactor index.py and initialize modules
    - Added init_app functions for sources, context, and vector_store
    - Created create_app function to encapsulate module initializations and app configurations
    - Set logging level to DEBUG
    
    this commit was generated by lguibr/comai

--- api/index.py
+++ api/index.py
@@ -1,6 +1,23 @@
+from sources import init_app as init_sources
+from context import init_app as init_context
+from vector_store import init_app as init_vector_store
 from flask import Flask
-app = Flask(__name__)
+import logging
 
-@app.route("/api/python")
-def hello_world():
-    return "<p>Hello, World!</p>"
+
+def create_app():
+    app = Flask(__name__)
+
+    # Initialize all your modules
+    init_sources(app)
+    init_context(app)
+    init_vector_store(app)
+    app.logger.setLevel(logging.DEBUG)
+
+    return app
+
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(debug=True)
